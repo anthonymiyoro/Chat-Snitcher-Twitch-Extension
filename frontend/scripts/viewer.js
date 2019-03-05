8e3e066a5273d448b761be1095123d67e6a08c8b
Anthony Miyoro <amiyoro@gmail.com>
2019-03-05 14:12:01 +0300

    removed logging and updated readme

--- frontend/scripts/viewer.js
+++ frontend/scripts/viewer.js
@@ -10,34 +10,34 @@ $(function() {
 
     // WebSocket Handlers
     socket.on('connect', () => {
-        console.log("Socket connected successfully, my Socket ID is " + socket.id);
+        // console.log("Socket connected successfully, my Socket ID is " + socket.id);
     });
     socket.on('connect_error', (err) => {
-        console.log("Failed to connect to socket:", err);
+        // console.log("Failed to connect to socket:", err);
     });
     socket.on('connect_timeout', (timeout) => {
-        console.log("Socket connection timed out:", timeout);
+        // console.log("Socket connection timed out:", timeout);
     });
     socket.on('error', (err) => {
-        console.log("Socket error:", err);
+        // console.log("Socket error:", err);
     });
     socket.on('disconnect', (reason) => {
-        console.log("Socket disconnected:", reason);
+        // console.log("Socket disconnected:", reason);
     });
     socket.on('reconnect', (attemptNumber) => {
-        console.log("Successfully reconnected to socket after " + attemptNumber + " attempts");
+        // console.log("Successfully reconnected to socket after " + attemptNumber + " attempts");
     });
     socket.on('reconnect_attempt', (attemptNumber) => {
-        console.log("Attempting reconnect... " + attemptNumber);
+        // console.log("Attempting reconnect... " + attemptNumber);
     });
     socket.on('reconnect_error', (err) => {
-        console.log("Failed to reconnect to socket:", err);
+        // console.log("Failed to reconnect to socket:", err);
     });
     socket.on('test', (msg) => {
-        console.log("New socket 'test' message:", msg);
+        // console.log("New socket 'test' message:", msg);
     });
     socket.on('whisper', (msg) => {
-        console.log("New socket 'whisper' message:", msg);
+        // console.log("New socket 'whisper' message:", msg);
     });
 
 
@@ -55,7 +55,7 @@ $(function() {
         if (!firstTimeOnly) {
             // Listen to this viewer's private PubSub whisper channel
             twitch.listen('whisper-'+latestAuth.userId, (target, type, msg) => {
-                console.log("New Twitch PubSub whisper:", msg);
+                // console.log("New Twitch PubSub whisper:", msg);
             });
         } else {
             setTimeout(whisperHack, 1000);
@@ -65,10 +65,10 @@ $(function() {
 
     // onAuth handler. Gives us JWT and the viewer's opaque ID
     twitch.onAuthorized((auth) => {
-        console.log("Twitch: onAuthorized called");
-        console.log("The channel ID is", auth.channelId);
+        // console.log("Twitch: onAuthorized called");
+        // console.log("The channel ID is", auth.channelId);
         // console.log("The extension clientId is", auth.clientId);
-        console.log("My Twitch opaque user id is", auth.userId);
+        // console.log("My Twitch opaque user id is", auth.userId);
         // console.log("The JWT token is", auth.token);
 
         // var channel_Id = auth.channelId.toString();
@@ -94,21 +94,19 @@ $(function() {
             // Open the websocket
             socket.open();
         }
-        // run function that sets the username of streamer
-        getStreamerName(auth.channelId);
        
     });
 
     // Sub all viewers to the broadcast channel
     twitch.listen('broadcast', (target, type, msg) => {
-        console.log("New Twitch PubSub broadcast message:", msg);
+        // console.log("New Twitch PubSub broadcast message:", msg);
     });
 
 
     // Error handler
     twitch.onError((err) => {
-        console.log("Twitch: onError called");
-        console.log("The error was", err);
+        // console.log("Twitch: onError called");
+        // console.log("The error was", err);
     });
     // onContext handler. Providers viewer mode, resolution, delay and other stuff
     // This can be very spammy, commented out by default
@@ -159,7 +157,7 @@ var getGlobalChannelName = (function(global) {
 document.getElementById("startLogging()").addEventListener("click", function(){
     var streamer_id = document.getElementById("streamerID").value;
     var form_id = document.getElementById("formDiv");
-    console.log("streamer_id", streamer_id);
+    // console.log("streamer_id", streamer_id);
     setGlobalChannelName(streamer_id);
     $.ajax({
         type: "POST",
@@ -171,7 +169,7 @@ document.getElementById("startLogging()").addEventListener("click", function(){
         },
         error: function(jqXHR, textStatus, err) {
             // alert();
-            console.log('text status '+textStatus+', err '+err);
+            // console.log('text status '+textStatus+', err '+err);
         }
     }); 
     form_id.style.display='none';
@@ -185,6 +183,13 @@ document.getElementById("ignoreForm()").addEventListener("click", function(){
     form_id.style.display='none';
     startWorker()
   });
+
+
+// If it is a normal viewer watching start worker 
+document.getElementById("ignoreForm()").addEventListener("click", function(){
+    startWorker()
+  });
+
 
 
 // Worker that checks the sentiment of comments every 5 seconds
